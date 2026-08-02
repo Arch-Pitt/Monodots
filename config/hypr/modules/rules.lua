63bd9d25c39454c82d04b0c6cf7439fafd890ba2
Pitt <pittestranhobizarro@gmail.com>
2026-08-01 22:58:33 -0300

    changing transparency type

--- config/hypr/modules/rules.lua
+++ config/hypr/modules/rules.lua
@@ -6,13 +6,6 @@ hl.window_rule({
     float = true,
 })
 
--- Transparency rules
-hl.window_rule({
-    name = "transparent-kitty",
-    match = { class = "kitty" },
-    opacity = 0.85,
-})
-
 -- Floating rules
 hl.window_rule({
     name = "floating-pavucontrol",
