38ab00568063bd3a2806fbbb54a055950294d05f
Pitt <pittestranhobizarro@gmail.com>
2026-08-19 20:54:45 -0300

    little tweaks(fuck quickshell)

--- config/hypr/modules/autostart.lua
+++ config/hypr/modules/autostart.lua
@@ -4,9 +4,9 @@ hl.on("hyprland.start", function()
     hl.exec_cmd("systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
 
     hl.exec_cmd("awww-daemon &")
-    hl.exec_cmd("waybar")
     hl.exec_cmd("swaync")
     hl.exec_cmd("swayosd-server")
+    hl.exec_cmd("waybar")
 
     hl.exec_cmd("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
 end)
