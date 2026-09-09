e70021a621cd5af34ae1fea9a8488282c8e238be
Pitt <pittestranhobizarro@gmail.com>
2026-09-09 17:24:25 -0300

    removendo o setup de wallpaper do awww

--- install.sh
+++ install.sh
@@ -135,6 +135,4 @@ sudo systemctl enable sddm
 
 systemctl --user daemon-reexec
 
-awww img ~/Pictures/Wallpapers/Monowaves.png
-
 echo "Dependencies are installed. Please follow the post-installation steps to complete the setup"
