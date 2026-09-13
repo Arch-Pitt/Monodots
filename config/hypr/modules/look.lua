aeb227c56b9437bcec931b0146339ba1b1fd5a0a
Pitt <pittestranhobizarro@gmail.com>
2026-09-12 23:39:20 -0300

    esqueci do border radius

--- config/hypr/modules/look.lua
+++ config/hypr/modules/look.lua
@@ -3,7 +3,7 @@ hl.config({
     general = {
         gaps_in = 3,
         gaps_out = 6,
-        border_size = 2,
+        border_size = 1,
 
         col = {
             active_border = "rgba(44444444)",
